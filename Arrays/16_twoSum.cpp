825901df5c617ab0cb6296776ee663197c855f98
Piyush Krishna Ojha <piyusho.ug21.cs@nitp.ac.in>
2024-07-11 12:07:49 +0530

    2Sum and 3Sum

---
+++ Arrays/16_twoSum.cpp
@@ -0,0 +1,30 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // using hashmap]
+        vector<int>ans;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if(mp.find(target - nums[i]) == mp.end()){
+                mp.insert({nums[i], i});
+            }else{
+                ans.push_back(mp[target - nums[i]]);
+                ans.push_back(i);
+                break;
+            }
+        }
+        return ans;
+    }
+};
